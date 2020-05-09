7ec51852986a3941a1f8a244822a24b48f08dd4e
Christian Vuerings <cvuerings@pinterest.com>
2020-05-09 15:50:35 -0700

    Initial Commit

---
+++ BrowserSaver/BrowserSaverView.h
@@ -0,0 +1,13 @@
+//
+//  BrowserSaverView.h
+//  BrowserSaver
+//
+//  Created by Christian Vuerings on 5/9/20.
+//  Copyright © 2020 Christian Vuerings. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BrowserSaverView : ScreenSaverView
+
+@end
---
+++ BrowserSaver/BrowserSaverView.m
@@ -0,0 +1,52 @@
+//
+//  BrowserSaverView.m
+//  BrowserSaver
+//
+//  Created by Christian Vuerings on 5/9/20.
+//  Copyright © 2020 Christian Vuerings. All rights reserved.
+//
+
+#import "BrowserSaverView.h"
+
+@implementation BrowserSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
